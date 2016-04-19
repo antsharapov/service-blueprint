ac0cbac737f6f167c87f831d1726c037fd7f0523
antsharapov <antsharapov@gmail.com>
2016-04-19 13:42:48 +0300

    19_04_1342

--- app/src/main/java/aero/basel/aaq/serviceblueprint/MainActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/MainActivity.java
@@ -1,13 +1,18 @@
 package aero.basel.aaq.serviceblueprint;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void NextButtonClick(View theButton)
+    {
+
     }
 }

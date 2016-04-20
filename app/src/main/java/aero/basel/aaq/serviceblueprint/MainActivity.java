f6e5be03fbb99813bc7d7223d773084fef1180aa
antsharapov <antsharapov@gmail.com>
2016-04-21 01:30:09 +0300

    21_04_1-29

--- app/src/main/java/aero/basel/aaq/serviceblueprint/MainActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/MainActivity.java
@@ -3,39 +3,40 @@ package aero.basel.aaq.serviceblueprint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
-import android.view.ViewDebug;
-import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.content.res.Resources;
 
 public class MainActivity extends Activity {
 
-
-
-    String[] questions;
-    String[] answers;
-
-    int i,j;
+    String[] questions, answers;
+    int i=0,j=0,k=0;
+    RadioGroup rg;
+    View checked_rb;
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((RadioGroup)findViewById(R.id.RadioGroup)).check(R.id.ZeroRadioButton);
+        ((RadioGroup)findViewById(R.id.RadioGroup)).check(-1);
         ((Chronometer) findViewById(R.id.chronometer)).setBase(SystemClock.elapsedRealtime());
-        ((TextView) findViewById(R.id.question)).setTextSize(24);
+        ((TextView) findViewById(R.id.question)).setTextSize(20);
         questions = getResources().getStringArray(R.array.questions);
         i = questions.length;
-        j=0;
         ((TextView) findViewById(R.id.question)).setText(questions[j]);
         findViewById(R.id.prev_button).setEnabled(false);
     }
 
     public void NextButtonClick(View theButton)
         {
+            rg = (RadioGroup) findViewById(R.id.RadioGroup);
+            k = rg.getCheckedRadioButtonId();
+            checked_rb = rg.findViewById(k);
+            k = rg.indexOfChild(checked_rb);
+            //TODO sum
+            rg.check(-1);
+            //answers[j]= String.valueOf(k);
             if (j<i-1) {
                 j++;
                 ((TextView) findViewById(R.id.question)).setText(questions[j]);
@@ -43,9 +44,12 @@ public class MainActivity extends Activity {
             }
             else
             {
-                ((TextView) findViewById(R.id.question)).setText("Finished");
+                ((TextView) findViewById(R.id.question)).setText(R.string.finished);
                 findViewById(R.id.next_button).setEnabled(false);
                 findViewById(R.id.prev_button).setEnabled(false);
+                ((Chronometer) findViewById(R.id.chronometer)).setActivated(false);
+                ((Chronometer) findViewById(R.id.chronometer)).stop();
+                ((SwitchCompat) findViewById(R.id.TimerSwitch)).setChecked(false);
             }
         }
 

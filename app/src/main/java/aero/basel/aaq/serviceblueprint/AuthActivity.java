569e690a69110a4c6034b4b9eb098cbc3cbb9c8b
antsharapov <antsharapov@gmail.com>
2016-04-22 18:17:15 +0300

    22_04_1815

--- app/src/main/java/aero/basel/aaq/serviceblueprint/AuthActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/AuthActivity.java
@@ -2,7 +2,13 @@ package aero.basel.aaq.serviceblueprint;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AuthActivity extends Activity {
 
@@ -11,6 +17,13 @@ public class AuthActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+        ArrayAdapter<String> emptyAdapter;
+        String[] strings = new String[] {};
+        List<String> items = new ArrayList<String>(Arrays.asList(strings));
+        emptyAdapter = new ArrayAdapter<String>(getBaseContext(),
+                android.R.layout.simple_spinner_item, items);
+        Spinner sp = (Spinner) findViewById(R.id.airports_spinner);
+        sp.setAdapter(emptyAdapter);
         TextView tv = (TextView) findViewById(R.id.airport_spiner_label);
         tv.setText(R.string.airport);
 

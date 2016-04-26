f868d2d99495b9aa972dcdc65fb6e2589aca7d35
antsharapov <antsharapov@gmail.com>
2016-04-26 13:18:03 +0300

    26_04_1317

--- app/src/main/java/aero/basel/aaq/serviceblueprint/AuthActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/AuthActivity.java
@@ -2,9 +2,12 @@ package aero.basel.aaq.serviceblueprint;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,19 +16,26 @@ import java.util.List;
 public class AuthActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        ArrayAdapter<String> emptyAdapter;
-        String[] strings = new String[] {};
-        List<String> items = new ArrayList<String>(Arrays.asList(strings));
-        emptyAdapter = new ArrayAdapter<String>(getBaseContext(),
-                android.R.layout.simple_spinner_item, items);
-        Spinner sp = (Spinner) findViewById(R.id.airports_spinner);
-        sp.setAdapter(emptyAdapter);
-        TextView tv = (TextView) findViewById(R.id.airport_spiner_label);
-        tv.setText(R.string.airport);
 
-    }
+        Spinner spinner = (Spinner) findViewById(R.id.airports_spinner);
+        CustomAdapter adapter = new CustomAdapter(this,
+                android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.airports));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                // Set adapter flag that something has been chosen
+                CustomAdapter.flag = true;
+            }
+        });
+
+     }
 }

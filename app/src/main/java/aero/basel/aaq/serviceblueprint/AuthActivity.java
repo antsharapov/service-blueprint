c3969e1b0eb3e41aaf05f08a2c95e071ec3def91
antsharapov <antsharapov@gmail.com>
2016-04-26 22:40:17 +0300

    2604_22-40

--- app/src/main/java/aero/basel/aaq/serviceblueprint/AuthActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/AuthActivity.java
@@ -16,20 +16,58 @@ import java.util.List;
 
 public class AuthActivity extends Activity {
 
-    ListView airportlv, servicelv, commissionlv;
+    ListView airport_lv, service_lv, commission_lv;
+    CharSequence airport, service, commission;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        airportlv = (ListView) findViewById(R.id.AirportListView);
-        airportlv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+//*******************Airport ListView init&click
+        airport_lv = (ListView) findViewById(R.id.AirportListView);
+        airport_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
                                     long id) {
                 Toast.makeText(getApplicationContext(), ((TextView) itemClicked).getText(),
                         Toast.LENGTH_SHORT).show();
-                airportlv.setVisibility(View.GONE);
+                airport = ((TextView) itemClicked).getText();
+                airport_lv.setVisibility(View.GONE);
+                service_lv.setVisibility(View.VISIBLE);
             }
         });
+//**********************************************
+
+// *******************Service ListView init&click
+        service_lv = (ListView) findViewById(R.id.ServiceListView);
+        service_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
+                                    long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) itemClicked).getText(),
+                        Toast.LENGTH_SHORT).show();
+                service = ((TextView) itemClicked).getText();
+                service_lv.setVisibility(View.GONE);
+                commission_lv.setVisibility(View.VISIBLE);
+            }
+        });
+//**********************************************
+
+
+// *******************Commission ListView init&click
+        commission_lv = (ListView) findViewById(R.id.CommissionListView);
+        commission_lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
+                                    long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) itemClicked).getText(),
+                        Toast.LENGTH_SHORT).show();
+                commission = ((TextView) itemClicked).getText();
+                commission_lv.setVisibility(View.GONE);
+            }
+        });
+//**********************************************
+        service_lv.setVisibility(View.GONE);
+        commission_lv.setVisibility(View.GONE);
      }
 }

be45aa8c90f32e6e83c75781ffda17fb85fc0869
antsharapov <antsharapov@gmail.com>
2016-05-03 18:26:29 +0300

    0305_18-25_disclaimer

--- app/src/main/java/aero/basel/aaq/serviceblueprint/DisclaimerActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/DisclaimerActivity.java
@@ -2,9 +2,12 @@ package aero.basel.aaq.serviceblueprint;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 public class DisclaimerActivity extends Activity
@@ -23,7 +26,33 @@ public class DisclaimerActivity extends Activity
         next_button.setEnabled(false);
 
         disclaimer = (TextView) findViewById(R.id.disclaimer);
-        disclaimer.setText("disclaimer");
+        disclaimer.setText(R.string.disclaimer);
+
+        agreed_cb = (CheckBox) findViewById(R.id.disclaimer_agreed_checkbox);
+        agreed_cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                if (isChecked){
+                    next_button.setEnabled(true);
+                }
+                else
+                {
+                    next_button.setEnabled(false);
+                }
+            }
+        }
+        );
+
+        next_button.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View view) {
+                                               Intent intent = new Intent(DisclaimerActivity.this, AuthActivity.class);
+                                               startActivity(intent);
+                                               finish();
+                                           }
+                                       }
+
+        );
 
     }
 

7eedbd52b40bb76de3d1968c97b60a0a44588b06
antsharapov <antsharapov@gmail.com>
2016-05-04 10:32:47 +0300

    0405_10-32

---
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/WhoActivity.java
@@ -0,0 +1,13 @@
+package aero.basel.aaq.serviceblueprint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class WhoActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_who);
+    }
+}

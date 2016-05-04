e86f0031892af836e1878379b2241d7009a58bb4
antsharapov <antsharapov@gmail.com>
2016-05-04 09:36:15 +0300

    0405_9-36

---
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/AppearanceActivity.java
@@ -0,0 +1,16 @@
+package aero.basel.aaq.serviceblueprint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class AppearanceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appearance);
+
+    }
+}

d41f2a67507e0c4d508541364561a652a5126c63
antsharapov <antsharapov@gmail.com>
2016-05-05 13:05:45 +0300

    0505_13-05

--- app/src/main/java/aero/basel/aaq/serviceblueprint/AppearanceActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/AppearanceActivity.java
@@ -27,27 +27,23 @@ public class AppearanceActivity extends Activity {
         setContentView(R.layout.activity_appearance);
 
         agent_spinner = (Spinner) findViewById(R.id.spinner);
-
         myDbHelper = new DatabaseHelper(this);
         try {
             myDbHelper.createDataBase();
         } catch (IOException ioe) {
             throw new Error("Unable to create database");
         }
-
         try {
             myDbHelper.openDataBase();
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
-
         SQLiteDatabase sdb = myDbHelper.getReadableDatabase();
         sdb.close();
         sdb = myDbHelper.getReadableDatabase();
-
-
-        String query = "Select name from agents";
-        Cursor cursor = sdb.rawQuery(query, null);
+        String airport = getIntent().getStringExtra("airport");
+        String query = "Select name from agents where airport = ?";
+        Cursor cursor = sdb.rawQuery(query, new String[] { airport });
         ArrayList<String> spinnerContent = new ArrayList<String>();
         if(cursor.moveToFirst()){
             do{
@@ -56,35 +52,10 @@ public class AppearanceActivity extends Activity {
             }while(cursor.moveToNext());
         }
         cursor.close();
-
         String[] allSpinner = new String[spinnerContent.size()];
         allSpinner = spinnerContent.toArray(allSpinner);
-
-
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(AppearanceActivity.this,android.R.layout.simple_spinner_item, allSpinner);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         agent_spinner.setAdapter(spinnerAdapter);
-        agent_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                return;
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        /*Cursor cursor = sdb.query(DatabaseHelper.DATABASE_USERS_TABLE, new String[]{DatabaseHelper.USERNAME_USERS_COLUMN,
-                        DatabaseHelper.PASSWORD_USERS_COLUMN, DatabaseHelper.REALNAME_USERS_COLUMN},
-                null, null,
-                null, null, null);
-
-        while (cursor.moveToNext()) {
-
-            //db_username = cursor.getString(cursor.getColumnIndex(DatabaseHelper.USERNAME_USERS_COLUMN));
-            //db_pass = cursor.getString(cursor.getColumnIndex(DatabaseHelper.PASSWORD_USERS_COLUMN));
-
-            }*/
         }
     }

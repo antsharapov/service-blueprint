4d25304681c24e74e23cc46061b5d6d4a45294c3
antsharapov <antsharapov@gmail.com>
2016-07-12 15:34:48 +0300

    1207_15-25-net_test

--- app/src/main/java/aero/basel/aaq/serviceblueprint/DisclaimerActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/DisclaimerActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -58,6 +62,7 @@ public class DisclaimerActivity extends Activity
                                                HttpURLConnection connection;
                                                OutputStreamWriter request = null;
                                                URL url = null;
+                                                           String response = null;
                                                String parameters = "name=test&mail=test@dummy.ru";
 
                                                    url = new URL("http://sd-glpi.basel.aero.local/phpmyadmin/and.php");
@@ -69,6 +74,20 @@ public class DisclaimerActivity extends Activity
                                                    request.write(parameters);
                                                    request.flush();
                                                    request.close();
+                                                           String line = "";
+                                                           InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+                                                           BufferedReader reader = new BufferedReader(isr);
+                                                           StringBuilder sb = new StringBuilder();
+                                                           while ((line = reader.readLine()) != null)
+                                                           {
+                                                               sb.append(line + "\n");
+                                                           }
+                                                           // Response from server after login process will be stored in response variable.
+                                                           response = sb.toString();
+                                                           // You can perform UI operations here
+                                                           //Toast.makeText(DisclaimerActivity.this,"Message from Server: \n"+ response, Toast.LENGTH_LONG).show();
+                                                           isr.close();
+                                                           reader.close();
                                                        }
                                                        catch (Exception e)
                                                        {

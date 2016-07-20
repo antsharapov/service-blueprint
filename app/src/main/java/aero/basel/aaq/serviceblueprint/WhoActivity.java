bcc246158f9ab1b0a918d5af15e4503a1e632531
antsharapov <antsharapov@gmail.com>
2016-07-20 11:22:35 +0300

    2007_11-10-qrcode

--- app/src/main/java/aero/basel/aaq/serviceblueprint/WhoActivity.java
+++ app/src/main/java/aero/basel/aaq/serviceblueprint/WhoActivity.java
@@ -2,6 +2,7 @@ package aero.basel.aaq.serviceblueprint;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,13 +33,38 @@ public class WhoActivity extends Activity {
                 GlobalVariables.person_flight_date = date_field.getText().toString();
 
                 if (!GlobalVariables.person_name.isEmpty() && !GlobalVariables.person_flight.isEmpty() && !GlobalVariables.person_flight_date.isEmpty()) {
-                    Intent intent = new Intent(WhoActivity.this, /*AgentSelection*/QRCodeActivity.class);
+                    /*Intent intent = new Intent(WhoActivity.this, /*AgentSelection//QRCodeActivity.class);
                     startActivity(intent);
-                    finish();
+                    finish(); */
+                    try {
+                        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                        intent.putExtra("SCAN_MODE", "SCAN_MODE");
+                        startActivityForResult(intent, 0);
+                        Toast.makeText(WhoActivity.this, "Отсканируйте код агента", Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+                        Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+                        startActivity(marketIntent);
+                    }
                 } else {
                     Toast.makeText(getApplicationContext(), R.string.empty_fields, Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+            String barcode = result.getStringExtra("SCAN_RESULT");
+            //String type = result.getStringExtra("SCAN_RESULT_FORMAT");
+            GlobalVariables.agent_name = barcode;
+            Intent intent = new Intent (WhoActivity.this,AppearanceActivity.class);
+            startActivity(intent);
+            finish();
+            //Toast.makeText(this, String.format("barcode: '%s' type: %s", barcode, type), Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+        }
     }
 }
